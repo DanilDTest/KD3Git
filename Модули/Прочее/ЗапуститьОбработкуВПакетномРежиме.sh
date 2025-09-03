158dc6e4e3cf90c452c0c27297bfd1f4b2a18a62
DanilD <158293193+DanilDTest@users.noreply.github.com>
2025-09-03 16:22:58 +0300

    Фикс запуска разбора пустой обработки

--- Модули/Прочее/ЗапуститьОбработкуВПакетномРежиме.sh
+++ Модули/Прочее/ЗапуститьОбработкуВПакетномРежиме.sh
@@ -5,5 +5,10 @@ Usr="$4"
 Pwd="$5"
 LaunchParameter="$6"
 
+if [ -z "$EPFPath" ]; then
+  exit 1
+fi
+
+
 "$PlatformPath" ENTERPRISE //IBName"$IBName" //N"$Usr" //P"$Pwd" //C"$LaunchParameter" //DisableStartupMessages //DisableStartupDialogs //DisableSplash //WA+ //AU- //Execute "$EPFPath" 
 
--- Модули/Разборка/РазборОбработкиНаИсходникиЧерезКонфигуратор.sh
+++ Модули/Разборка/РазборОбработкиНаИсходникиЧерезКонфигуратор.sh
@@ -1,7 +1,11 @@
 ResCatalogPath=$1 ##Куда выгрузить результат разбора
 EPFPath=$2 ##Обработка для разбора
 LogPath=$3 
-OneSPath=$4
+OneSPath=$4 
+
+if [ -z "$EPFPath" ]; then
+  exit 1
+fi
 
 cd "$OneSPath"
 #./1cv8.exe DESIGNER //DumpExternalDataProcessorOrReportToFiles "D:\1с\Очоба\OS\test\TestUnpack\Res" "D:\1с\Очоба\OS\test\TestUnpack\T.epf" /Out "D:\1с\Очоба\OS\test\TestUnpack\Res\out.txt"
