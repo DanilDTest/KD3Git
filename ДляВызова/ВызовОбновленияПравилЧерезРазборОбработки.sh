3cf9ef910790cb65d6429a5582029ae63a981170
ArdiukovDV <145449772+iDavlar@users.noreply.github.com>
2026-06-23 14:20:05 +0300

    Фикс выбора файлов с пробелами

--- ДляВызова/ВызовОбновленияПравилЧерезРазборОбработки.sh
+++ ДляВызова/ВызовОбновленияПравилЧерезРазборОбработки.sh
@@ -7,10 +7,15 @@ commit="Обновление правил $gitRepoName"
 cd "$RulesCatPath"
 
 echo "Обработка для разбора"
-select fname in $(find . -type d -name "Рабочие" -prune -o -type f -name "*.epf" -print0 | xargs -0 -r ls -t | head -n 10);
-do
+
+# 1. Читаем вывод команды построчно и сохраняем в массив file_list
+mapfile -t file_list < <(find . -type d -name "Рабочие" -prune -o -type f -name "*.epf" -print0 | xargs -0 -r ls -t | head -n 10)
+
+# 2. Передаем массив в select. 
+# Конструкция "${file_list[@]}" с кавычками гарантирует, что пробелы внутри имен файлов не разорвут строку.
+select fname in "${file_list[@]}"; do
     if [ -n "$fname" ]; then
-        EPFPath="$RulesCatPath/$fname"
+        EPFPath="$RulesCatPath/${fname#./}"
         break;
     else
         echo "Неверный выбор. Пожалуйста, попробуйте снова."
