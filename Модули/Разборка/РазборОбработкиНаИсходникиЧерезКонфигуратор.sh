b8735a854e39f9d97cc824bb7139b86c15ddd73a
ArdiukovDV <145449772+iDavlar@users.noreply.github.com>
2026-06-23 15:59:11 +0300

    Исправление запуска конфигуратор

--- Модули/Разборка/РазборОбработкиНаИсходникиЧерезКонфигуратор.sh
+++ Модули/Разборка/РазборОбработкиНаИсходникиЧерезКонфигуратор.sh
@@ -7,7 +7,16 @@ if [ -z "$EPFPath" ]; then
   exit 1
 fi
 
-cd "$OneSPath"
+#cd "$OneSPath"
 #./1cv8.exe DESIGNER //DumpExternalDataProcessorOrReportToFiles "D:\1с\Очоба\OS\test\TestUnpack\Res" "D:\1с\Очоба\OS\test\TestUnpack\T.epf" /Out "D:\1с\Очоба\OS\test\TestUnpack\Res\out.txt"
-./1cv8.exe DESIGNER //DumpExternalDataProcessorOrReportToFiles "$ResCatalogPath" "$EPFPath" -Plain /Out "$LogPath"
+#./1cv8.exe DESIGNER //DumpExternalDataProcessorOrReportToFiles "$ResCatalogPath" "$EPFPath" -Plain /Out "$LogPath"
 
+# 1. Переводим все пути в понятный для Windows формат (D:\...)
+WinResCatalogPath=$(cygpath -w "$ResCatalogPath")
+WinEPFPath=$(cygpath -w "$EPFPath")
+WinLogPath=$(cygpath -w "$LogPath")
+
+cd "$OneSPath"
+
+# 2. Обязательно берем все Windows-пути в двойные кавычки!
+./1cv8.exe DESIGNER //DumpExternalDataProcessorOrReportToFiles "$WinResCatalogPath" "$WinEPFPath" -Plain /Out "$WinLogPath"
